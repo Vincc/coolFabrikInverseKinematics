3bc8da8554288362ec4fde237014d9a3c8df0e10
Vincent Chang <changvincent57@gmail.com>
2021-09-28 13:57:48 +0800

    cleaned up

--- FABRIK.py
+++ FABRIK.py
@@ -4,7 +4,7 @@ import math
 size = (700,700)
 
 #Change jointCount, and the allowed radius of movement
-jointCount = 10
+jointCount = 30
 radius = 400
 
 #length and starting position of "arm"
@@ -17,12 +17,8 @@ screen=pygame.display.set_mode(size)
 done = False
 mouse_position = joints[-1]
 
-def getDirec(point1, point2):
-    angle = math.atan2(point1[1]-point2[1], point1[0]-point2[0])
-    dis = math.sqrt((point1[1]-point2[1])**2 + (point1[0]-point2[0])**2)
-    if dis > radius:
-        dis = radius
-    return angle, dis
+def getAngle(point1, point2):
+    return math.atan2(point1[1]-point2[1], point1[0]-point2[0])
     
 def drawArms(joints):
     pygame.draw.lines(screen, (255,255,255), closed=False, points=joints, width=1)
@@ -31,7 +27,7 @@ def drawArms(joints):
 
 def updateJoint(oldEndind, newPos, itern):
     oldEnd = joints[oldEndind]
-    angle, dis = getDirec(oldEnd,newPos)
+    angle = getAngle(oldEnd,newPos)
     x, y = math.cos(angle)*length, math.sin(angle)*length
     newEnd = (newPos[0]+x,newPos[1]+y)
     if itern == 1:
@@ -46,18 +42,10 @@ while not done:
             done = True
         if event.type == pygame.MOUSEMOTION:
             mouse_position = pygame.mouse.get_pos()
-            for i in range(3):
-                newjoints = [mouse_position]
-                updateJoint(len(joints)-2,mouse_position,len(joints)-1)
+            for i in [mouse_position, startpos]:
+                newjoints = [i]
+                updateJoint(len(joints)-2,i,len(joints)-1)
                 joints = newjoints
-                newjoints = [startpos]
-                updateJoint(len(joints)-2,startpos,len(joints)-1)
-                joints = newjoints
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                if event.key == pygame.K_SPACE:
-                    angle, dis = getDirec(mouse_position,startpos)
-                    print(angle/math.pi, dis)
                     
     screen.fill((0,0,0))
     drawArms(joints)
